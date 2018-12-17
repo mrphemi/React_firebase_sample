bacf22f7c8ce2bb7e71e0f02926c32e9cf776cf9
mrphemi <mrlapinho@gmail.com>
2018-12-17 22:51:31 +0100

    Setup app structure, Added important components and installed react-router

---
+++ config/fire.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const config = {
+   apiKey: "AIzaSyDG7pu0TrL6YG75HHN2nB20GCY9a9ghtfU",
+   authDomain: "sample-firebase-project-f8b3e.firebaseapp.com",
+   databaseURL: "https://sample-firebase-project-f8b3e.firebaseio.com",
+   projectId: "sample-firebase-project-f8b3e",
+   storageBucket: "sample-firebase-project-f8b3e.appspot.com",
+   messagingSenderId: "867298145773"
+};
+firebase.initializeApp(config);
---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Navigation from "../Navigation";
+
+const App = () => (
+   <div>
+      <Navigation />
+   </div>
+);
+
+export default App;
