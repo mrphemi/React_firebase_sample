cbbd374fecd62754703359597aebe4e694f31e27
mrphemi <mrlapinho@gmail.com>
2018-12-18 00:16:36 +0100

    Added navigation and implemented routes using react-router.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 const Navigation = () => (
    <div>
-      <h2>Navigation</h2>
+      <nav className="navbar is-primary">
+         <div className="navbar-brand">
+            <span className="navbar-item">
+               <Link to={ROUTES.LANDING}>RFB</Link>
+            </span>
+            <a
+               role="button"
+               class="navbar-burger burger"
+               aria-label="menu"
+               aria-expanded="false"
+               data-target="mainMenu"
+               href
+            >
+               <span aria-hidden="true" />
+               <span aria-hidden="true" />
+               <span aria-hidden="true" />
+            </a>
+         </div>
+         <div id="mainMenu" class="navbar-menu">
+            <div className="navbar-end">
+               <span className="navbar-item">
+                  <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+               </span>
+               <span className="navbar-item">
+                  <Link to={ROUTES.HOME}>Home</Link>
+               </span>
+               <span className="navbar-item">
+                  <Link to={ROUTES.ACCOUNT}>Account</Link>
+               </span>
+               <span className="navbar-item">
+                  <Link to={ROUTES.ADMIN}>Admin</Link>
+               </span>
+            </div>
+         </div>
+      </nav>
    </div>
 );
 
